5e2384786f2861dc35f8cf8e13646ae0f4a375c7
cyl <15534010320@163.com>
2022-06-07 22:14:00 +0800

    剑指 Offer 04. 二维数组中的查找[解法2]

--- er-wei-shu-zu-zhong-de-cha-zhao-lcof.go
+++ er-wei-shu-zu-zhong-de-cha-zhao-lcof.go
@@ -21,6 +21,30 @@ func findNumberIn2DArray(matrix [][]int, target int) bool {
 	return false
 }
 
+//  findNumberIn2DArray 解法2
+//  @Description:
+//  @param matrix
+//  @param target
+//  @return bool
+func findNumberIn2DArray2(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+
+	cow, cel := 0, len(matrix[0])-1
+	for cow < len(matrix) && cel >= 0 {
+		if matrix[cow][cel] == target {
+			return true
+		} else if matrix[cow][cel] > target {
+			cel--
+		} else {
+			cow++
+		}
+	}
+
+	return false
+}
+
 func main() {
 
 }
