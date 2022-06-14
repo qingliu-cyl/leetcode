2229996d370c91da513bb44b572e3c9710c80f6a
cyl <15534010320@163.com>
2022-06-14 22:21:53 +0800

    剑指 Offer 52. 两个链表的第一个公共节点

---
+++ liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof.go
@@ -0,0 +1,25 @@
+package main
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headB == nil || headA == nil {
+		return nil
+	}
+
+	p1, p2 := headA, headB
+
+	for p1 != p2 {
+		if p1 == nil {
+			p1 = headB
+		} else {
+			p1 = p1.Next
+		}
+
+		if p2 == nil {
+			p2 = headA
+		} else  {
+			p2 = p2.Next
+		}
+	}
+
+	return p1
+}
