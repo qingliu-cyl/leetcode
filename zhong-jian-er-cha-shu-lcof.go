660e98ca81407acf265b809c4eee24b2b1c716e0
cyl <15534010320@163.com>
2022-06-24 21:06:56 +0800

    剑指 Offer 07. 重建二叉树

---
+++ zhong-jian-er-cha-shu-lcof.go
@@ -0,0 +1,72 @@
+package main
+
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	root := &TreeNode{
+		Val: preorder[0],
+	}
+
+	findFitst := func(list []int) int {
+		for _, i := range preorder {
+			for j := 0; j < len(list); j++ {
+				if list[j] == i {
+					return i
+				}
+			}
+		}
+
+		return -1
+	}
+
+	var findChild func(list []int, target *TreeNode)
+	findChild = func(list []int, target *TreeNode) {
+		if len(list) == 0 {
+			return
+		}
+		// 根据中序遍历获取左节点
+		var left []int
+		var right []int
+		for idx, _ := range list {
+			if list[idx] == target.Val {
+				left = list[:idx]
+				right = list[idx+1:]
+			}
+		}
+
+		if len(left) > 0 {
+			leftVal := findFitst(left)
+			leftNode := &TreeNode{
+				Val: leftVal,
+			}
+			target.Left = leftNode
+			findChild(left, leftNode)
+		}
+
+		if len(right) > 0 {
+			rightVal := findFitst(right)
+			rightNode := &TreeNode{
+				Val: rightVal,
+			}
+			target.Right = rightNode
+			findChild(right, rightNode)
+		}
+
+	}
+
+	findChild(inorder, root)
+	return root
+}
+
+func main() {
+	buildTree([]int{7,-10,-4,3,-1,2,-8,11}, []int{-4,-10,3,-1,7,11,-8,2})
+}
