ba042348c22096d1d9275f0a8207690d7638fee9
cyl <15534010320@163.com>
2022-07-30 17:27:21 +0800

    插入排序

--- insertion_sort.go
+++ insertion_sort.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// 插入排序
 func insterionSert(list []int) {
 	if len(list) <= 1 {
 		return
@@ -16,6 +17,21 @@ func insterionSert(list []int) {
 		}
 
 		list[j+1] = v
+	}
+}
+
+// 插入排序2
+func insertSort(nums []int){
+	tmp := make([]int, len(nums))
+	for i := 1; i < len(nums); i++ {
+		j := i-1
+		for j >= 0  && nums[j] > nums[i]{
+			j--
+		}
+
+		copy(tmp, nums)
+		nums[j+1] = nums[i]
+		copy(nums[j+2:i+1], tmp[j+1:i])
 	}
 }
 
